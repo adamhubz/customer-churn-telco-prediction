e69ef32d4fc60cc4b169ff61c62fb0fc9c33598d
adamhubz <78313862+adamhubz@users.noreply.github.com>
2026-03-22 11:05:27 +0700

    Update 2_Problem.py

--- pages/2_Problem.py
+++ pages/2_Problem.py
@@ -2,22 +2,27 @@ import streamlit as st
 
 st.set_page_config(page_title = 'Problem', page_icon=':iphone:', layout='wide')
 st.title('Problem')
-st.write("""The development of the telecommunications industry is very rapid, which can be seen from people's behavior in using the internet for communication.
 
-This behavior has led to the emergence of many telecommunications companies and an increase in internet service providers, resulting in competition among providers.
+tab1, tab2 = st.tabs(["🇮🇩 Bahasa Indonesia", "🇬🇧 English"])
 
-Customers have the right to choose the provider that suits them and can switch from their previous provider, which is referred to as Customer Churn.
+with tab1:
+  st.write("""The development of the telecommunications industry is very rapid, which can be seen from people's behavior in using the internet for communication.
+  
+  This behavior has led to the emergence of many telecommunications companies and an increase in internet service providers, resulting in competition among providers.
+  
+  Customers have the right to choose the provider that suits them and can switch from their previous provider, which is referred to as Customer Churn.
+  
+  This switching can lead to a decrease in revenue for telecommunications companies, making it important to address.
+  
+  From this issue, it can be concluded that companies want to identify customers who have the potential to become Customer Churn, which can reduce revenue for telecommunications companies. Therefore, companies can take preventive actions toward customers who are likely to churn. This can be done using a machine learning classification method, specifically supervised learning, by building a machine learning model to classify whether customers will churn or not based on the dataset we have.""")
 
-This switching can lead to a decrease in revenue for telecommunications companies, making it important to address.
-
-From this issue, it can be concluded that companies want to identify customers who have the potential to become Customer Churn, which can reduce revenue for telecommunications companies. Therefore, companies can take preventive actions toward customers who are likely to churn. This can be done using a machine learning classification method, specifically supervised learning, by building a machine learning model to classify whether customers will churn or not based on the dataset we have.""")
-
-st.write("""Perkembangan industri telekomunikasi sangatlah cepat, hal ini dapat dilihat dari perilaku masyarakat yang menggunakan internet dalam berkomunikasi.
-
-Perilaku ini menyebabkan banyaknya perusahaan telekomunikasi dan meningkatnya internet service provider yang dapat menimbulkan persaingan antar provider.
-
-Pelanggan memiliki hak dalam memilih provider yang sesuai dan dapat beralih dari provider sebelumnya yang diartikan sebagai Customer Churn.
-
-Peralihan ini dapat menyebabkan berkurangnya pendapatan bagi perusahaan telekomunikasi sehingga penting untuk ditangani.
-
-Dari permasalahan ini bisa disimpulkan bahwa perusahaan ingin mengetahui pelanggan yang berpotensi menjadi Customer Churn yang dapat menyebabkan berkurangnya pendapatan bagi perusahaan telekomunikasi, sehingga perusahaan tersebut dapat melakukan tindakan preventif terhadap pelanggan yang berpotensi menjadi Customer Churn. Hal ini dapat dilakukan dengan menggunakan metode machine learning klasifikasi supervised learning yaitu membangun model machine learning untuk mengklasifikasi pelanggan yang menjadi Customer Churn atau tidak berdasarkan dataset yang kita miliki.""")
+with tab2:
+  st.write("""Perkembangan industri telekomunikasi sangatlah cepat, hal ini dapat dilihat dari perilaku masyarakat yang menggunakan internet dalam berkomunikasi.
+  
+  Perilaku ini menyebabkan banyaknya perusahaan telekomunikasi dan meningkatnya internet service provider yang dapat menimbulkan persaingan antar provider.
+  
+  Pelanggan memiliki hak dalam memilih provider yang sesuai dan dapat beralih dari provider sebelumnya yang diartikan sebagai Customer Churn.
+  
+  Peralihan ini dapat menyebabkan berkurangnya pendapatan bagi perusahaan telekomunikasi sehingga penting untuk ditangani.
+  
+  Dari permasalahan ini bisa disimpulkan bahwa perusahaan ingin mengetahui pelanggan yang berpotensi menjadi Customer Churn yang dapat menyebabkan berkurangnya pendapatan bagi perusahaan telekomunikasi, sehingga perusahaan tersebut dapat melakukan tindakan preventif terhadap pelanggan yang berpotensi menjadi Customer Churn. Hal ini dapat dilakukan dengan menggunakan metode machine learning klasifikasi supervised learning yaitu membangun model machine learning untuk mengklasifikasi pelanggan yang menjadi Customer Churn atau tidak berdasarkan dataset yang kita miliki.""")
